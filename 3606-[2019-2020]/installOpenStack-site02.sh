e95bc4aa455fd21883dd5dae8bd0b0d74ec2e967
Naresh Seegobin <33396127+NareshSeegobin@users.noreply.github.com>
2020-01-13 11:16:47 -0400

    Create installOpenStack-site02.sh

---
+++ 3606-[2019-2020]/installOpenStack-site02.sh
@@ -0,0 +1,33 @@
+!#/bin/bash
+
+## Based on https://www.mirantis.com/blog/how-to-install-openstack-on-your-local-machine-using-devstack/
+## It is essential to create a stack user with sudo access or else you will get a HOTS_IP error:
+## https://stackoverflow.com/questions/46192965/error-in-running-stack-sh-in-devstack
+
+sudo useradd -s /bin/bash -d /opt/stack -m stack
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
+
+sudo su - stack
+
+
+### git clone https://github.com/openstack-dev/devstack.git -b stable/pike devstack/
+### Use latest stable as of 20200113 for latest Phyton versions
+git clone https://github.com/openstack-dev/devstack.git -b stable/train devstack/
+
+cd devstack/
+
+cat >  local.conf <<EOF
+[[local|localrc]]
+ADMIN_PASSWORD=secret
+DATABASE_PASSWORD=\$ADMIN_PASSWORD
+RABBIT_PASSWORD=\$ADMIN_PASSWORD
+SERVICE_PASSWORD=\$ADMIN_PASSWORD
+HOST_IP=10.0.2.15
+RECLONE=yes
+EOF
+
+./stack.sh
+
+
+
+
