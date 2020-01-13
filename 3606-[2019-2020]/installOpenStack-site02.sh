76da9249268e1653ba0f3d75c2a9acaafd53b26b
Naresh Seegobin <33396127+NareshSeegobin@users.noreply.github.com>
2020-01-13 11:19:52 -0400

    Update installOpenStack-site02.sh

--- 3606-[2019-2020]/installOpenStack-site02.sh
+++ 3606-[2019-2020]/installOpenStack-site02.sh
@@ -4,6 +4,10 @@
 ## It is essential to create a stack user with sudo access or else you will get a HOTS_IP error:
 ## https://stackoverflow.com/questions/46192965/error-in-running-stack-sh-in-devstack
 
+## Revision History:
+## 20200113-01 - NS - First version
+
+
 sudo useradd -s /bin/bash -d /opt/stack -m stack
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 
